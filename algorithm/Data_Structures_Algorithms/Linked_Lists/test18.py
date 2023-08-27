11a997750b456a960df4b7be688ed4d444bd680f
Soeun Uhm <uhmturks@yonsei.ac.kr>
2023-08-27 23:41:18 +0900

    test18 fix

--- algorithm/Data_Structures_Algorithms/Linked_Lists/test18.py
+++ algorithm/Data_Structures_Algorithms/Linked_Lists/test18.py
@@ -72,10 +72,8 @@ my_linked_list.append(3)
 my_linked_list.append(3)
 my_linked_list.append(2)
 my_linked_list.append(4)
-#my_linked_list.remove_duplicates()
+my_linked_list.remove_duplicates()
 
-my_set = my_linked_list.remove_duplicates()
-print(my_set)
 
 my_linked_list.print_all()
 
