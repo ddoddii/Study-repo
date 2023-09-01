4e4890f37e521702300a088ac3a45bba3c93e88c
Soeun Uhm <uhmturks@yonsei.ac.kr>
2023-09-01 19:33:51 +0900

    S&Q

---
+++ algorithm/Data_Structures_Algorithms/Stacks&Queues/test46.py
@@ -0,0 +1,3 @@
+# Queue Using Stacks: Enqueue
+# You are given a class MyQueue which implements a queue using two stacks. 
+# Your task is to implement the enqueue method which should add an element to the back of the queue.
---
+++ algorithm/Data_Structures_Algorithms/Stacks&Queues/test47.py
@@ -0,0 +1,3 @@
+# Queue Using Stacks: Dequeue
+# You have been tasked with implementing a queue data structure using two stacks in Python, and you need to write the dequeue method
+# The dequeue method should remove and return the first element in the queue.
